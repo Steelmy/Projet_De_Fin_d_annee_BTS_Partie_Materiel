8589b6c8671ceea782aadf8bed33665d1f978027
Steelmy <liamorymigeon@gmail.com>
2026-03-16 15:45:21 +0100

    Refactor: Streamlined sidebar styling by removing custom component CSS, adjusting menu item alignment, and adding new text and flex utilities.

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -1,12 +1,12 @@
-<nav class="sidebar text-center font-bold" id="sidebar">
+<nav class="sidebar font-bold" id="sidebar">
     <div class="sidebar-header">
         <button class="close-sidebar-btn" onclick="toggleSidebar()">&times;</button>
-        <h2 class="text-custom-brandLight font-bold">INVENTAIRE</h2>
+        <h2 class="text-custom-brandLight font-bold text-2xl">INVENTAIRE</h2>
     </div>
     <ul class="sidebar-menu">
-        <li class="flex justify-center"><a href="../dashboard.php">Dashboard</a></li>
-        <li class="flex justify-center"><a href="../dashboard?page=users">Utilisateurs</a></li>
-        <li class="active flex justify-center"><a href="index.php">Gestion du matériel</a></li>
-        <li onclick="toggleModal(true)" class="text-custom-brandLight font-bold mt-4 cursor-pointer flex justify-center">Nouvel Emprunt</li>
+        <li class="flex justify-start"><a href="../dashboard.php">Dashboard</a></li>
+        <li class="flex justify-start"><a href="../dashboard?page=users">Utilisateurs</a></li>
+        <li class="active flex justify-start"><a href="index.php">Gestion du matériel</a></li>
+        <li onclick="toggleModal(true)" class="text-custom-brandLight font-bold mt-4 cursor-pointer flex justify-start">Nouvel Emprunt</li>
     </ul>
 </nav>
